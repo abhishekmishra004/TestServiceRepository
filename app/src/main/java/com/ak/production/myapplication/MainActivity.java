57bad09b9c15e7803a0b74dd9da995c09d7d4368
Abhishek <abhishekmishragacr@gmail.com>
2021-04-13 17:20:17 +0530

    setup

---
+++ app/src/main/java/com/ak/production/myapplication/MainActivity.java
@@ -0,0 +1,125 @@
+package com.ak.production.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.app.NotificationCompat;
+import androidx.core.widget.ImageViewCompat;
+
+import android.app.ActivityManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RemoteViews;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    CardView cvMain;
+    ImageView imageViewCompat;
+    Button btnStart,btnStop;
+
+    NotificationIntentService notificationIntentService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cvMain = findViewById(R.id.cv_main);
+        imageViewCompat = findViewById(R.id.iv_close);
+        btnStart = findViewById(R.id.btn_start_service);
+        btnStop = findViewById(R.id.btn_stop_service);
+
+        Intent serviceIntent = new Intent(this,NotificationIntentService.class);
+        bindService(serviceIntent, connection, BIND_AUTO_CREATE);
+
+        imageViewCompat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cvMain.setVisibility(View.GONE);
+            }
+        });
+
+
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent serviceIntent = new Intent(MainActivity.this,NotificationIntentService.class);
+                serviceIntent.setAction("START");
+
+                boolean running = isMyServiceRunning(NotificationIntentService.class);
+                Log.d("notifiIntent", "onClick: service running or not-"+running);
+               // if(running) return;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    startForegroundService(serviceIntent);
+                } else {
+                    startService(serviceIntent);
+                }
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*Intent serviceIntent = new Intent(MainActivity.this,NotificationIntentService.class);
+                stopService(serviceIntent);*/
+                notificationIntentService.onDestroy();
+                /*final ActivityManager.RunningServiceInfo serviceInfo = getRunningServiceInfo(NotificationIntentService.class, MainActivity.this);
+                if (serviceInfo != null) {
+                    android.os.Process.killProcess(serviceInfo.pid); //Stop the running service
+                }*/
+            }
+        });
+
+    }
+
+    ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            notificationIntentService = ((NotificationIntentService.NotificationBinder) service).getService();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(connection);
+    }
+
+    public static ActivityManager.RunningServiceInfo getRunningServiceInfo(Class serviceClass, Context context) {
+        ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return service;
+            }
+        }
+        return null;
+    }
+
+
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/com/ak/production/myapplication/NotificationIntentService.java
@@ -0,0 +1,149 @@
+package com.ak.production.myapplication;
+
+import android.app.IntentService;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+import java.io.IOException;
+
+public class NotificationIntentService extends Service {
+
+    private String NOTIFICATION_TITLE = "Notification Sample App";
+    private String CONTENT_TEXT = "Expand me to see a detailed message!";
+    MediaPlayer player;
+
+    final IBinder notificationBinder = new NotificationBinder();
+
+    public class NotificationBinder extends Binder {
+        public  NotificationIntentService getService(){
+            return NotificationIntentService.this;
+        }
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return notificationBinder;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        player = MediaPlayer.create(this, R.raw.jawaanijaaneman);
+        player.setVolume(100,100);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        sendNotification();
+        onHandleIntent(intent);
+       // player.start();
+        return START_STICKY;
+    }
+
+
+
+    protected void onHandleIntent(Intent intent) {
+        if(null == intent || intent.getAction() == null) return;
+        Log.d("notifiIntent", "onHandleIntent: action-"+intent.getAction());
+        switch (intent.getAction()) {
+            case "left":
+                Handler leftHandler = new Handler(Looper.getMainLooper());
+                leftHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getBaseContext(), "You clicked the left button", Toast.LENGTH_LONG).show();
+                    }
+                });
+                break;
+            case "right":
+                Handler rightHandler = new Handler(Looper.getMainLooper());
+                rightHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getBaseContext(), "You clicked the right button", Toast.LENGTH_LONG).show();
+                    }
+                });
+                break;
+            case "STOP" :
+                stopForeground(true);
+                if(player.isPlaying()) player.pause();
+                stopSelf();
+                break;
+            case "START" :
+                player.start();
+                break;
+        }
+    }
+
+
+    public void sendNotification() {
+
+        Log.d("notifiIntent", "sendNotification: inside sned notification");
+
+        RemoteViews expandedView = new RemoteViews(getPackageName(), R.layout.layout_expaneded_notification);
+        expandedView.setTextViewText(R.id.tv_heading, "dynamic expanded song name");
+
+        Intent leftIntent = new Intent(this, NotificationIntentService.class);
+        leftIntent.setAction("left");
+        expandedView.setOnClickPendingIntent(R.id.iv_play_pause, PendingIntent.getService(this, 0, leftIntent, PendingIntent.FLAG_UPDATE_CURRENT));
+
+
+        RemoteViews collapsedView = new RemoteViews(getPackageName(), R.layout.layout_collapsed_notification);
+        collapsedView.setTextViewText(R.id.tv_heading, "dynamic collapsed song name");
+
+        Intent rightIntent = new Intent(this, NotificationIntentService.class);
+        rightIntent.setAction("right");
+        collapsedView.setOnClickPendingIntent(R.id.iv_play_pause, PendingIntent.getService(this, 0, rightIntent, PendingIntent.FLAG_UPDATE_CURRENT));
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+
+            NotificationChannel channel = new NotificationChannel("serviceChannelId", "serviceChannelName", NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setSound(null, null);
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"serviceChannelId")
+                // these are the three things a NotificationCompat.Builder object requires at a minimum
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle(NOTIFICATION_TITLE)
+                .setContentText(CONTENT_TEXT)
+                // notification will be dismissed when tapped
+                .setAutoCancel(true)
+                // tapping notification will open MainActivity
+                .setContentIntent(PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0))
+                // setting the custom collapsed and expanded views
+                .setCustomContentView(collapsedView)
+                .setCustomBigContentView(expandedView);
+                // setting style to DecoratedCustomViewStyle() is necessary for custom views to display
+               // .setStyle(new NotificationCompat.BigPictureStyle());
+
+        // retrieves android.app.NotificationManager
+
+        startForeground(120, builder.build());
+    }
+
+
+    @Override
+    public void onDestroy() {
+        stopForeground(true);
+        if(null != player && player.isPlaying()) player.pause();
+        super.onDestroy();
+    }
+}
